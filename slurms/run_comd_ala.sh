7d98d1f863e13af2de82ce52951488ae6e8048f0
wesleygomersall <gomersallw@gmail.com>
2025-07-07 20:36:45 -0700

    update slurm batch

--- slurms/run_comd_ala.sh
+++ slurms/run_comd_ala.sh
@@ -2,19 +2,20 @@
 
 # Wesley Gomersall
 # Mon, 07 Jul 2025 19:21:29 -0700
+# Mon, 07 Jul 2025 20:32:35 -0700
 #
-# Script to launch slurm jobs on Talapas:
+# Launch slurm jobs on Talapas:
 # Run steered molecular dynamics openmm script
+# Triplicate of ComD-C16 and triplicate of ComD-alanines
 
 SCRIPT=/home/wesg/openmm_scripts/slurms/50ns_smd.sbatch
+OUTPUTDIR=/home/wesg/openmmscripts 
+PULLFORCE=5
 
-sbatch $SCRIPT firstarg secondarg thirdarg
-sbatch $SCRIPT firstarg secondarg thirdarg
-sbatch $SCRIPT firstarg secondarg thirdarg
+sbatch $SCRIPT /home/wesg/proteinpdbs/ComD_C16_AF3_fixed.pdb $OUTPUTDIR c16_1 $PULLFORCE
+sbatch $SCRIPT /home/wesg/proteinpdbs/ComD_C16_AF3_fixed.pdb $OUTPUTDIR c16_2 $PULLFORCE
+sbatch $SCRIPT /home/wesg/proteinpdbs/ComD_C16_AF3_fixed.pdb $OUTPUTDIR c16_3 $PULLFORCE
 
-sbatch $SCRIPT firstarg secondarg thirdarg
-sbatch $SCRIPT firstarg secondarg thirdarg
-sbatch $SCRIPT firstarg secondarg thirdarg
-
-
-
+sbatch $SCRIPT /home/wesg/proteinpdbs/ComD_C16_AF3_allalanine_fixed.pdb $OUTPUTDIR ala_1 $PULLFORCE
+sbatch $SCRIPT /home/wesg/proteinpdbs/ComD_C16_AF3_allalanine_fixed.pdb $OUTPUTDIR ala_2 $PULLFORCE
+sbatch $SCRIPT /home/wesg/proteinpdbs/ComD_C16_AF3_allalanine_fixed.pdb $OUTPUTDIR ala_3 $PULLFORCE
