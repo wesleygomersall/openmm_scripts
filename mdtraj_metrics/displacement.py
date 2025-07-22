598bb7194dbfdb449039bc793bb54bd5bb555b2d
wesleygomersall <gomersallw@gmail.com>
2025-07-22 00:05:04 -0700

    displacement calc for each frame in traj

--- mdtraj_metrics/displacement.py
+++ mdtraj_metrics/displacement.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 
+import mdtraj as md 
+import numpy as np
 from tools import *
 
+def peptide_displacement(trajectory):
+    '''
+    Track the displacement of peptide (chainid == 1) throughout a trajectory.
+    Displacement is calculated from the first frame of trajectory. 
+    
+    Input: 
+        mdtraj Trajectory object
+    Output: 
+        tuple: List of center of mass coordinates, List of displacement scalars
+    '''
+    com = md.compute_center_of_mass(mytraj, select="chainid == 1")
+    displacement = [np.linalg.norm(np.subtract(c, com[0])) for c in com]
+    return com, displacement
+
 if __name__ == "__main__":
-    mytraj = SmdTrajectory(args.input)
-    myref = Reference(args.ref, mytraj)
+    mytraj = md.load(args.input).remove_solvent()
+    mytraj = prepend_reference(mytraj, md.load(args.ref))
+    
+    coords, disp = peptide_displacement(mytraj)
 
-    peptide_displacement = []
-
-    # Create csv with columns: Frame, Displacement
-    with open(args.output.split('.csv')[0] + ".csv", 'w') as out: 
-        out.write("Frame,Displacement(nm)\n")
-        for i, d in enumerate(peptide_displacement): 
-            out.write(f"{i+1},{d}\n")
+    for i in range(len(coords)):
+        if i == 0: print("Step,Displacement(nm),Coordinates")
+        else: 
+            print(i, disp[i], coords[i], sep=",")
