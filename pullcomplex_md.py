9618a777a9577e0ed5a5c79c593131e04a679c8e
wesleygomersall <gomersallw@gmail.com>
2025-08-13 22:14:02 -0700

    nonperiodic system with spherical container

--- pullcomplex_md.py
+++ pullcomplex_md.py
@@ -19,7 +19,6 @@ parser.add_argument("--temp", type=float, default=300.0, help="Temperature (Kelv
 parser.add_argument("--pull-force", type=float, default=25.0, help="Pull force constant which will be applied to the peptide.")
 parser.add_argument("--add-water", action="store_true", help="Add this option to populate modeller with a surrounding box of water molecules.")
 parser.add_argument("--suppress-movement", action="store_true", help="Add this option to hold atoms in the protein chain in place throughout the simulation. If specified, program will look for file called 'hold.txt' containing residue numbers to hold stationary. 'hold.txt' should contain integer IDs separated by lines and/or spaces.") 
-parser.add_argument("--periodic", action="store_true", help="Add this option to use PME for nonbonded interaction, enforcing a periodic box on the trajectory.") 
 args = parser.parse_args()
 
 def center_of_mass(pos, system, atoms): 
@@ -58,7 +57,7 @@ def custom_force(direction, atoms, force_constant):
     fy = direction[1]
     fz = direction[2]
 
-    pull = CustomCentroidBondForce(1, "force_constant * (x1*fx + y1*fy + z1*fz)")
+    pull = CustomCentroidBondForce(1, "-1 * force_constant * (x1*fx + y1*fy + z1*fz)")
     pull.addGlobalParameter("force_constant", force_constant)
     pull.addGlobalParameter("fx", fx)
     pull.addGlobalParameter("fy", fy)
@@ -68,40 +67,28 @@ def custom_force(direction, atoms, force_constant):
 
     return pull
 
-def restraining_force(direction, atoms, anchor_point, distance, force_constant):
+def container_restraint(atoms, radius: float, force_constant: float = 100.00):
     '''
-    Define force which is applied only if there is sufficient space between the two groups.  
+    Define force to contain peptide chain to a spherical container. 
 
     Input(s): 
-        direction (list):       (x, y, z) vector for direction of force.
-        atoms (str):            List of atoms ids to apply force to.
-        anchor_point (str):     (x, y, z) coordinate for the point to restrain around. 
-        distance (list):        Distance in nm before restraining force is applied.
+        atoms (list):           List of atoms ids to apply force to.
+        radius (float):         Radius of container in nm. 
         f_constant (float):     Constant force * kcal/mole/A
 
     Output(s): 
-        openmm.CustomCentroidBondForce object
+        openmm.CustomExternalForce object
     '''
     force_constant *= kilocalories_per_mole / angstroms
-    # distance *= nanometer
+    radius *= nanometer
+    force_expression = "container_force*max(0, r - radius)^2; r = sqrt(x^2 + y^2 + z^2) "
+    container = CustomExternalForce(force_expression)
+    container.addGlobalParameter("radius", radius) 
+    container.addGlobalParameter("container_force", force_constant) 
+    for p in atoms: 
+        container.addParticle(p, [])
+    return container
 
-    fx = direction[0]
-    fy = direction[1]
-    fz = direction[2]
-
-    restrain = CustomCentroidBondForce(1, "step(sqrt((x1-t0_x)^2 + (y1-t0_y)^2 + (z1-t0_z)^2) - distance) * -1 * force_constant * (x1*fx + y1*fy + z1*fz)")
-    restrain.addGlobalParameter("force_constant", force_constant)
-    restrain.addGlobalParameter("fx", fx)
-    restrain.addGlobalParameter("fy", fy)
-    restrain.addGlobalParameter("fz", fz)
-    restrain.addGlobalParameter("t0_x", anchor_point[0] * nanometer)
-    restrain.addGlobalParameter("t0_y", anchor_point[1] * nanometer)
-    restrain.addGlobalParameter("t0_z", anchor_point[2] * nanometer)
-    restrain.addGlobalParameter("distance", distance)
-    restrain.addGroup(atoms)
-    restrain.addBond([0])
-
-    return restrain
 
 if not os.path.exists(args.output):
     os.mkdir(args.output)
@@ -122,58 +109,39 @@ pdb = PDBFile(args.input)
 forcefield = ForceField('amber14-all.xml', 'amber14/tip3p.xml')
 modeller = Modeller(pdb.topology, pdb.positions)
 
-if len(list(modeller.topology.chains())) != 2:
-    # take care of possibly unbound protein and peptide list of residues
+if len(list(modeller.topology.chains())) != 2: 
     raise Exception("Topology does not have exactly two chains.")
     
 for i, c in enumerate(modeller.topology.chains()):
     if i == 0: protein = [res for res in c.residues()]
     if i == 1: peptide = [res for res in c.residues()]
 
-# For tracking peptide stability, get the second and the second-to-last alpha carbon ids
-CA_1_id, CA_2_id = 0, 0
-for resnum, res in enumerate(peptide): 
-    if resnum == 1:
-        for atom in res.atoms(): 
-            if atom.name == 'CA': 
-                CA_1_id = atom.index
-    if resnum == len(peptide) - 2:
-        for atom in res.atoms(): 
-            if atom.name == 'CA': 
-                CA_2_id = atom.index
 
 log.write("Adding hydrogen atoms.\n")
 modeller.addHydrogens(forcefield)
 
 # Optionally explicit water solvent
 if args.add_water: 
-    log.write("Adding solvent: water\n")
+    log.write("Add water solvent\n")
     modeller.addSolvent(forcefield, model='tip3p', 
                         positiveIon='Na+', negativeIon='Cl-',
                         padding= 1 * nanometer, 
                         neutralize=True)
 
 # create system needs to be after adding solvent and hydrogens
-if args.periodic:
-    log.write("nonbondedMethod: PME\n")
-    system = forcefield.createSystem(modeller.topology, 
-                                    nonbondedMethod=PME, 
-                                    nonbondedCutoff=1*nanometer, 
-                                    removeCMMotion=False,
-                                    constraints=HBonds) # remove this line if error with constraints involving zero-mass atoms
-if not args.periodic: 
-    log.write("nonbondedMethod: CutoffNonPeriodic\n")
-    system = forcefield.createSystem(modeller.topology, 
-                                    nonbondedMethod=app.CutoffNonPeriodic,
-                                    nonbondedCutoff=1*nanometer, 
-                                    removeCMMotion=False,
-                                    constraints=HBonds) # remove this line if error with constraints involving zero-mass atoms
+system = forcefield.createSystem(modeller.topology, 
+                                 nonbondedMethod=app.CutoffNonPeriodic, 
+                                 nonbondedCutoff=1*nanometer, 
+                                 removeCMMotion=False,
+                                 constraints=HBonds) # remove this line if error with constraints involving zero-mass atoms
 
 if args.suppress_movement: 
     # Optionally change mass of some residues' atoms to 0 to suppress movement
     # Warning: 
     #   This was causing N-terminal nitrogen atom of peptide to also remain locked 
     #   in place. Mass of that atom had not been set to zero.  
+
+    pass
 
     hold_residues = []
     with open('hold.txt', 'r') as holdfile:
@@ -204,10 +172,12 @@ peptide_atoms = [atom.index for residue in peptide for atom in residue.atoms()]
 pull_direction = t0_peptide_center - t0_protein_center
 pull_direction = pull_direction / np.linalg.norm(pull_direction) # make unit vector
 pullforce = custom_force(pull_direction, peptide_atoms, args.pull_force)
-restraint = restraining_force(pull_direction, peptide_atoms, t0_peptide_center, 8, args.pull_force)
 system.addForce(pullforce)
-system.addForce(restraint)
 log.write(f"Pull force {args.pull_force} kcal/mole/Angstrom added in {pull_direction} direction.")
+
+# Enforce a sphere boundary of radius 10nm
+sphere_container = container_restraint(range(system.getNumParticles()), 10, 100)
+system.addForce(sphere_container)
 
 integrator = LangevinIntegrator(temperature, 1/picosecond, tstep) 
 
@@ -223,9 +193,9 @@ simulation.minimizeEnergy()
 minimized_sim_energy = simulation.context.getState(getEnergy=True).getPotentialEnergy()
 log.write(f"Potential energy after minimization: {minimized_sim_energy}\n")
 
-log.write(f"Running simulation for {args.steps} steps.\n")
+log.write(f"{args.steps} steps\n")
 log.write(f"Time step: {tstep}\n")
-log.write(f"Running simulation, storing data every {store} iterations.\n")
+log.write(f"energy.csv rows = {store}\n")
 
 simulation.reporters.append(PDBReporter(output_pdb_path, args.steps // 100)) # only store 100 frames in trajectory pdb
 simulation.reporters.append(StateDataReporter(output_energy, 
