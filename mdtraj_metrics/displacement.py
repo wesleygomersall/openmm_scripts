a0a85c415f65422ef21cf47b0c843819adede4d8
wesleygomersall <gomersallw@gmail.com>
2025-08-13 22:29:13 -0700

    revert changes to displacement calculation

--- mdtraj_metrics/displacement.py
+++ mdtraj_metrics/displacement.py
@@ -15,37 +15,11 @@ def peptide_displacement(trajectory, reference):
     Output: 
 
     '''
+    com = md.compute_center_of_mass(trajectory, select="chainid == 1")
+    ref_com = md.compute_center_of_mass(reference, select="chainid == 1")
+    displacement = [np.linalg.norm(np.subtract(c, com[0])) for c in com]
+    return displacement
     
-    # Setup trajectory from tools 
-    check_ref_match(trajectory, reference)
-    centeredtraj = center_traj_pbc(trajectory) 
-
-    proteinCAs = centeredtraj.topology.select("(chainid == 0) and (name == CA)")
-    peptideCAs = centeredtraj.topology.select("(chainid == 1) and (name == CA)")
-    atom_pairs= []
-    for atom_a_idx in proteinCAs:
-        for atom_b_idx in peptideCAs:
-            atom_pairs.append([atom_a_idx, atom_b_idx])
-    atom_pairs= np.array(atom_pairs)
-
-    traj_disp = md.compute_displacements(centeredtraj, atom_pairs, periodic=True)
-    ref_disp = md.compute_displacements(reference, atom_pairs) 
-    traj_disp = np.linalg.norm(traj_disp, axis=2)
-    ref_disp = np.linalg.norm(ref_disp, axis=2)
-    distances = []
-
-    # First get distance in reference
-    distances.append(sum(ref_disp[0]) / len(ref_disp[0]))
-
-    # Append distance from each frame
-    for frame in range(centeredtraj.n_frames):
-        # print(frame)
-        # print(traj_disp[frame])
-        distances.append(sum(traj_disp[frame]) / len(traj_disp[frame]))
-
-    return distances
-
-
 if __name__ == "__main__":
     mytraj = md.load(args.input).remove_solvent()
     myref = md.load(args.ref)
