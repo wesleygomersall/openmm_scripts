8baca4ac30b5257087493923da24a54280262995
Wesley Gomersall <gomersallw@gmail.com>
2026-01-16 13:10:23 -0800

    handle .gz pdb

--- mdtraj_metrics/remove_solvent.py
+++ mdtraj_metrics/remove_solvent.py
@@ -8,7 +8,12 @@ def save_without_solv(trajectory_file_path):
     Save version of trajectory without any solvent. 
     '''
     my_traj = md.load(trajectory_file_path).remove_solvent()
-    outfilepath = trajectory_file_path.strip('.pdb') + "_solventRemoved.pdb"
+    if trajectory_file_path.endswith('.pdb'):
+        outfilepath = trajectory_file_path.strip('.pdb') + "_solventRemoved.pdb"
+    elif trajectory_file_path.endswith('.pdb.gz'):
+        outfilepath = trajectory_file_path.strip('.pdb.gz') + "_solventRemoved.pdb"
+    else: 
+        raise TypeError("{trajectory_file_path} file suffix not recognized. Use .pdb or .pdb.gz file.")
     my_traj.save_pdb(outfilepath, force_overwrite = True) 
     return 0 
 
